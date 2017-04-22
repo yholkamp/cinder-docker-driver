2c836c2399c3fc980e2d2796fabbd81d016223b9
Yorick Holkamp <github@nextpulse.nl>
2017-04-22 09:21:30 +0200

    Revised main.go to ensure all flags are defined before .Parse() is called

--- main.go
+++ main.go
@@ -19,20 +19,21 @@ var (
 
 func main() {
 	showVersion := flag.Bool("version", false, "Display version number of plugin and exit")
-	flag.Parse()
-	if *showVersion == true {
-		fmt.Println("Version: ", VERSION)
-		os.Exit(0)
-	}
-
 	cfgFile := flag.String("config", "/var/lib/cinder/dockerdriver/config.json", "path to config file")
 	debug := flag.Bool("debug", true, "enable debug logging")
 	flag.Parse()
+
 	if *debug == true {
 		log.SetLevel(log.DebugLevel)
 	} else {
 		log.SetLevel(log.InfoLevel)
 	}
+
+	if *showVersion == true {
+		fmt.Println("Version: ", VERSION)
+		os.Exit(0)
+	}
+
 	log.Info("Starting cinder-docker-driver version: ", VERSION)
 	d := New(*cfgFile)
 	h := volume.NewHandler(d)
